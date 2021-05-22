354812fa19b5672b27abdf21ee3806a5edb57e7b
cordobadigitalmedia <ismael@cordobadigitalmedia.com>
2021-05-22 07:20:05 +0300

    next config edit

--- next.config.js
+++ next.config.js
@@ -41,18 +41,3 @@ module.exports = withBundleAnalyzer({
     return config
   },
 })
-/**
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  }
-}
-*/
