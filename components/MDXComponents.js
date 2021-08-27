b697a7209d7daef5fb0b49cbad9826e3f7f04e55
cordobadigitalmedia <ismael@cordobadigitalmedia.com>
2021-08-27 18:01:59 +0300

    updated showVerses component to pass sura

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,25 +1,23 @@
 import Image from 'next/image'
 import CustomLink from './Link'
-import showVersesFrom93 from '@/data/quran/93'
 import showVersesFrom94 from '@/data/quran/94'
 import showVersesFrom95 from '@/data/quran/95'
 import showVersesFrom42 from '@/data/quran/42'
 import showVersesFrom48 from '@/data/quran/48'
 import showVersesFrom105 from '@/data/quran/105'
-import showVersesFrom6 from '@/data/quran/6'
 import VerseExplanation from '@/components/VerseExplanation'
 import FullAudioPlayer from '@/components/FullAudioPlayer'
 import Vocabulary from '@/components/Vocabulary'
 import MarkComplete from '@/components/MarkComplete'
 import { PrayerApplyTable } from '@/components/PrayerApplyTable'
+import { ShowVerses } from '@/components/ShowVerses'
 import { Counter } from '../store/Counter'
 import { PrayerChecker } from '../store/PrayerChecker'
 
 const MDXComponents = {
   Image,
   a: CustomLink,
-  showVersesFrom6,
-  showVersesFrom93,
+  ShowVerses,
   showVersesFrom94,
   showVersesFrom95,
   showVersesFrom42,
